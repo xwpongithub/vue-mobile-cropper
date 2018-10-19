2c7d5e6bf889eac6100fa70f9a0ac33d41467d8f
xfreedom <xwpjava@126.com>
2018-10-19 14:06:34 +0800

    import  cropper

--- src/js/vue-mobile-cropper.js
+++ src/js/vue-mobile-cropper.js
@@ -2,7 +2,44 @@ import Cropper from 'cropperjs'
 
 export default {
   name: 'vue-mobile-cropper',
+  props: {
+    aspectRatio: {
+      type: Number,
+      default: 1
+    },
+    autoCropArea: {
+      type: Number,
+      default: 0.8
+    },
+    viewMode : {
+      type: Number,
+      default: 1
+    }
+  },
+  computed: {
+    cropperOptions() {
+      return {
+        aspectRatio: this.aspectRatio,
+        autoCropArea: this.autoCropArea,
+        viewMode: this.viewMode,
+        guides : true ,
+        cropBoxResizable: true ,
+        cropBoxMovable: true ,
+        dragCrop: true ,
+        background: false,
+        checkOrientation: true ,
+        checkCrossOrigin: true ,
+        zoomable: false,
+        zoomOnWheel: false ,
+        center: false ,
+        toggleDragModeOnDblclick: false ,
+      }
+    }
+  },
   render(h) {
     return h('div', 'cropper component')
+  },
+  mounted() {
+
   }
 }
